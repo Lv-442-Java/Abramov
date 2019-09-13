7d517e293dca79278f13ea012965f41491c6af6a
NazarRyvak <ryvaknt96@gmail.com>
2019-09-13 20:44:28 +0300

    add task 330

--- src/chapter/one/NestedLoops.java
+++ src/chapter/one/NestedLoops.java
@@ -1,6 +1,7 @@
 package chapter.one;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class NestedLoops {
     public static ArrayList<Integer> task329(int n, int m) {
@@ -19,4 +20,41 @@ public class NestedLoops {
         }
         return result;
     }
+    
+    public static List<Integer> task330(int number) throws NumberFormatException {
+		List<Integer> perfectNumbers = new ArrayList<Integer>();
+		if (!isNaturalNumber(number)) {
+			throw new NumberFormatException("Incorrect data!!!The number isn`t natural!!!");
+		}
+		for (int i = 1; i < number; i++) {
+			if (isPerfect(i)) {
+				perfectNumbers.add(i);
+			}
+		}
+		return perfectNumbers;
+	}
+
+	private static boolean isPerfect(int number) {
+		int sum = 0;
+		for (int i = 1; i < number; i++) {
+			if (number % i == 0) {
+				sum += i;
+			}
+			if (sum > number) {
+				return false;
+			}
+		}
+		if (sum == number) {
+			return true;
+		}
+		return false;
+	}
+
+	private static boolean isNaturalNumber(int number) {
+		if (number > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
