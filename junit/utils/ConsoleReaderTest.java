acc1f4d0216e7cbb273a7607dabfdb6ab024f768
Nazar Hladysh <nazar.gladish@gmail.com>
2019-09-17 15:11:25 +0300

    update test class

---
+++ junit/utils/ConsoleReaderTest.java
@@ -0,0 +1,24 @@
+package utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsoleReaderTest {
+
+    @Test
+    void inputNaturalNumber() {
+    }
+
+    @Test
+    void inputArrayOfNaturalNumbers() {
+    }
+
+    @Test
+    void inputIntegerNumber() {
+    }
+
+    @Test
+    void inputDoubleNumber() {
+    }
+}
