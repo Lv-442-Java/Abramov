75c488329d7ae52d75ca50c924e538716f94cdf4
OlegAgrus <34576707+OlegAgrus@users.noreply.github.com>
2019-09-18 22:12:31 +0300

    add tests to task 559

--- junit/chapter/two/IntegerNumbersTest.java
+++ junit/chapter/two/IntegerNumbersTest.java
@@ -1,6 +1,10 @@
 package chapter.two;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,7 +15,28 @@ class IntegerNumbersTest {
     }
 
     @Test
-    void task559() {
+    void task559_testWithDefaultInput() {
+        int number = 40;
+
+        List<Integer> actual = IntegerNumbers.task559(number);
+
+        List<Integer> expected = new ArrayList<Integer>() {
+            {
+                add(3); add(7); add(31);
+            }
+        };
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void task559_testThatResultNumsMustBeLessThanMainNum() {
+        int number = 3;
+
+        List<Integer> actual = IntegerNumbers.task559(number);
+
+        List<Integer> expected = new ArrayList<Integer>();
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
