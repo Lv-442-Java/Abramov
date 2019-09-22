c559d46e1df12d917979ff2f5735a00c15ca5066
Nazar Hladysh <nazar.gladish@gmail.com>
2019-09-22 14:24:24 +0300

    add inputNaturalNumber test

--- junit/utils/ConsoleReaderTest.java
+++ junit/utils/ConsoleReaderTest.java
@@ -2,12 +2,22 @@ package utils;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ConsoleReaderTest {
 
     @Test
     void inputNaturalNumber() {
+        Scanner sc = mock(Scanner.class);
+        when(sc.hasNextInt()).thenReturn(true);
+        when(sc.nextInt()).thenReturn(5);
+        ConsoleReader cr = new ConsoleReader(sc);
+        Integer expect = 5;
+        assertEquals(expect,cr.inputIntegerNumber());
     }
 
     @Test
